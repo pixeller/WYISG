d56daa5be78a96ae9cfdf729d85391df22b52bb3
AlfredYeap <alfred.freelance@gmail.com>
2015-06-23 04:21:43 +0800

    Heritage thumbnails images compress

--- heritage-trails.php
+++ heritage-trails.php
@@ -41,7 +41,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/afamosa.jpg" data-toggle="lightbox" data-gallery="multiimages" title="The Porta De Santiago (A Famosa) is one of the most popular sites that you can find when visiting Malacca. It is a major historical landmark and is probably the most photographed structure in Malacca. The A Famosa is located just next to the Stadthuys building at Paremeswara Road – Melaka Town. Nearest museum are the “Muzium Dunia Melayu dan Dunia Islam” and “ Muzium Setem Melaka”.">
-                                        <img src="images/heritage-places/afamosa.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/afamosa.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>A Famosa</h4>
                                         </div>
@@ -53,7 +53,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/malacca-straits-mosque.jpg" data-toggle="lightbox" data-gallery="multiimages" title="Malacca Straits Mosque is a mosque located on the man-made Malacca Island near Malacca Town in Malacca state, Malaysia. It is situated facing the Malacca Straits on a 1.8 hectare land. It is the latest landmark showcasing the excellent and glorious modern Islamic Craftmanship.">
-                                        <img src="images/heritage-places/malacca-straits-mosque.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/malacca-straits-mosque.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>Malacca Straits Mosque</h4>
                                         </div>
@@ -66,7 +66,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/stadthuys.jpg" title="The legacy of the Dutch is still very much alive and felt in Malacca in a form of a red building called the Stadthuys. It used to be the administrative center of successive governments but now it is a history museum. The Stadthuys can be easily found as it is just beside the Christ Church. Opposite the Stadthuys is the famous Jonker Street visited by many tourists.">
-                                        <img src="images/heritage-places/stadthuys.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/stadthuys.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>Stadthuys</h4>
                                         </div>
@@ -79,7 +79,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/sultanatepalace.jpg" title="From the A Famosa or Porta de Santiago, you will pass by a gravesite for the Dutch colonist before reaching at the Malacca Sultanate Palace. It is situated at the foot of the famous St Paul's Hill. All these famous landmarks are all very close by each other, so it is impossible to miss the Malacca Sultanate Palace.">
-                                        <img src="images/heritage-places/sultanatepalace.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/sultanatepalace.jpg" class="img-responsive">
                                         <div class="desc">
                                     <h4>Malacca Sultanate Palace Museum</h4>
                                 </div>
@@ -96,7 +96,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/tamingsaritower.jpg" title="Taming Sari Tower located in the popular district of Malacca, Bandar Hilir on Jalan Merdeka, Taming Sari Tower Malacca is 3 minutes away from Dataran Pahlawan Megamall and Mahkota Parade Shopping Complex. The Central location of the tower is just 0.35km away from St. Paul's Hill (A Famosa). Situated near to local popular and renowned attractions, The Stadthuys The Stadthuys and Cheng Ho Culture Museum is located 0.42km and 0.45km away from Taming Sari Tower Malacca">
-                                        <img src="images/heritage-places/tamingsaritower.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/tamingsaritower.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>Taming Sari Tower</h4>
                                         </div>
@@ -108,7 +108,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/maritime-museum.jpg" title="The Maritime Museum of Malacca is located just ahead of the Stadthuys. It is exactly opposite of the Medan Samudera which is a handicraft and local produce building. The best way to get to the Maritime Museum from the Stadthuys is by walking as you can appreciate better the museums, galleries, mosques, churches and other historical fort around Banda Hilir.">
-                                        <img src="images/heritage-places/maritime-museum.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/maritime-museum.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>Malacca Maritime Museum</h4>
                                         </div>
@@ -121,7 +121,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/babanyonya.jpg" title="The Baba Nyonya Heritage Museum Melaka is located at Jalan Tun Tan Cheng Lock and Heern Street which is of 10.66km from Zoo Melaka. The museum is located parallel to Jonker Street, Malacca, is just 0.22km away from the renowned Jonker Walk. Across the street, Jalan Tun Tan Cheng Lock or better known as the Millionaire's Row is near to Cheng Ho's Cultural Museum that takes about 0.26km away. The central location of the heritage museum is near to Kampung Hulu's Mosque and St. Paul Hill (A'Famosa).">
-                                        <img src="images/heritage-places/babanyonya.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/babanyonya.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>Baba Nyonya Museum</h4>
                                         </div>
@@ -136,7 +136,7 @@
                                     <a href="images/heritage-places/jonkerwalk.jpg" title="Jonker Street is located in the heart of the Melaka city. Due to the fact that it is one of the biggest attractions in the state, Jonker Street’s location can be easily found via the various road signs visitors can see along the way when they travel to the place.
 
 ">
-                                        <img src="images/heritage-places/jonkerwalk.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/jonkerwalk.jpg" class="img-responsive">
                                         <div class="desc">
                                     <h4>Jonker Walk</h4>
                                 </div>
@@ -153,7 +153,7 @@
                             <div class="album">
                                 <div class="holder">
                                     <a href="images/heritage-places/chenghoonteng.jpg" title="Cheng Hoon Teng Temple is located in Jalan Tokong, also known as the Harmony Street. Set near the Hindu temple Sri Pogyatha Vinoyagar along Jalan Hang Lekiu (in Jonker Street). Directly opposite Cheng Hoon Teng Temple is a traditional Chinese opera theatre; beside the opera theatre is a newly constructed Xianglin temple. Also known as the Fragrant Forest Temple, this shrine follows the traditional layout of Chinese Buddhist temples.">
-                                        <img src="images/heritage-places/chenghoonteng.jpg" class="img-responsive">
+                                        <img src="images/heritage-places/thumbnail/chenghoonteng.jpg" class="img-responsive">
                                         <div class="desc">
                                             <h4>Cheng Hoon Teng Temple</h4>
                                         </div>
